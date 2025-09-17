efe0e8debee14606386bf9a0ac9ad7ef7d0bcf09
Ibtihash <syedibtihash@gmail.com>
2025-09-18 01:11:22 +0500

    fixed frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const BASE_URL = "https://quiz-game-uywu.vercel.app";
 
 const API = {
   async getQuestions(params = {}) {
